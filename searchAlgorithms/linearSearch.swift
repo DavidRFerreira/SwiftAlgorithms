b373d371b3f0c945214754addc5dbcb9f9985890
DavidRFerreira <53045049+DavidRFerreira@users.noreply.github.com>
2020-05-16 14:35:27 +0100

    Update linearSearch.swift

--- searchAlgorithms/linearSearch.swift
+++ searchAlgorithms/linearSearch.swift
@@ -1,16 +1,22 @@
-// Quick Sort Algorithm
+// Linear Search Algorithm
 
-/* Description of the Algorithm:
-        We search an element in a given array by traversing the array
-        from the starting, till the desired element is found.
+/* We search an element in a given array by traversing the array
+ * from the start till the desired element is found (or till the
+ * end of the array if the element is not present).
+ *
+ * In the worst case scenario, this algorithm takes O(n) time.
  */
+
+
 
 import Foundation
 
-let sizeOfArray : Int = 20
-let numberToSearch = 25 // change here if you want to search for other number.
 
+//*************************************
+//******** Auxiliary Functions ********
+//*************************************
 
+// Inserts random numbers into an array passed as an argument.
 func populateArrayRandomly(sizeArray: Int, numbers: inout [Int])
 {
     for i in 0...sizeArray-1
@@ -19,7 +25,7 @@ func populateArrayRandomly(sizeArray: Int, numbers: inout [Int])
     }
 }
 
-
+// Iterates over the array passed as an argument and prints all its values.
 func printArrayNumbers(numbers: [Int])
 {
     for number in numbers
@@ -29,16 +35,21 @@ func printArrayNumbers(numbers: [Int])
 }
 
 
+//****************************************
+//******** Linear Search Function ********
+//****************************************
+
 func linearSearch(numbers: inout[Int], numberToSearch: Int)
 {
     var indexPosition : Int = 0
     
+    // we iterate over the entire array (till the end or until we find the desired element).
     for number in numbers
     {
-        if (number == numberToSearch)
+        if (number == numberToSearch) // compare the element to search with each element present in the array.
         {
             print("\(numberToSearch) is present at the \(indexPosition) in the array.")
-            return
+            return // if the element is found, we finish the search.
         }
         
         indexPosition = indexPosition + 1
@@ -50,7 +61,13 @@ func linearSearch(numbers: inout[Int], numberToSearch: Int)
 
 
 
-//*** MAIN ***
+//**********************
+//******** MAIN ********
+//**********************
+
+let sizeOfArray : Int = 20 // the number of items in the desired array.
+let numberToSearch = 25 // the number we want to search.
+
 var arrayNumbers = [Int](repeating: 0, count:sizeOfArray)
 
 populateArrayRandomly(sizeArray: sizeOfArray, numbers: &arrayNumbers)
