b769a07655f6feb7edda85487543f42e1ed45189
DavidRFerreira <53045049+DavidRFerreira@users.noreply.github.com>
2020-05-16 14:44:39 +0100

    Update exponential.swift

--- recursiveAlgorithms/exponential.swift
+++ recursiveAlgorithms/exponential.swift
@@ -1,6 +1,15 @@
 // Algorithm to calculate the power of a number using recursion.
 
+/* Example: exponential(base: 2, exponent: 2)
+ * 2^2 = 2 * 2 = 4
+*/ Therefore, the output is going to be 4.
+
 import Foundation
+
+
+//*******************************************
+//******** Power Calculator Function ********
+//*******************************************
 
 func exponential(base : Int, exponent : Int) -> Int
 {
@@ -8,7 +17,7 @@ func exponential(base : Int, exponent : Int) -> Int
     {
         return (base * exponential(base: base, exponent: exponent - 1))
     }
-    else
+    else // a number power to zero is always going to be equal to one.
     {
         return 1
     }
@@ -16,7 +25,9 @@ func exponential(base : Int, exponent : Int) -> Int
 
 
 
-// ###### MAIN ######
+//**********************
+//******** MAIN ********
+//**********************
 
 print(exponential(base: 2, exponent: 2))
 
